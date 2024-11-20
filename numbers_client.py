0d6137237d4da8ec75653ad394c92edce54da9ea
noamc <noam.cohen@safebreach.com>
2024-11-20 17:52:42 +0200

    Add implementation of the client with sending the parsed commands

--- numbers_client.py
+++ numbers_client.py
@@ -1,10 +1,16 @@
+import re
 import sys
 import socket
 
 WELCOME_MESSAGE = "Welcome! Please log in."
-USER_NAME = "bob"
-PASSWORD = "simplepass"
-AUTH_MESSAGE = f"User: {USER_NAME}\nPassword: {PASSWORD}"
+
+# Matches on {calculate} at the beginning followed with {space} and then a {signed int} {space} {operation} {signed int}
+CALCULATE_COMMAND_REGEX = "^calculate: (-?\d{1,9}) (\^|\/|\*|-|\+) (-?\d{1,9})$"
+MAX_COMMAND_REGEX = "^max: \((-?\d+)(?: (-?\d+))*\)$"
+FACTORS_COMMAND_REGEX = "^factors: (-?\d+)$"
+QUIT = "quit"
+
+MESSAGE_SEP = "\\"
 
 def handle_auth(client_socket: socket.socket):
     """ This method is responsible for the authentication of the client against the server """
@@ -13,15 +19,39 @@ def handle_auth(client_socket: socket.socket):
 
     # Message received from server is auth message
     if resp  == WELCOME_MESSAGE:
-        client_socket.sendall(AUTH_MESSAGE.encode())
+        username = input("User: ")
+        password = input("Password: ")
+        client_socket.sendall(f"0 {username} Password: {password}{MESSAGE_SEP}".encode())
 
 
-def execute_command():
+def execute_command(client_socket: socket.socket):
     """
     Gets the command from user and parses it.
     If command is valid then it is sent to the server.
     """
-    pass
+    command = input("Please enter your command: ")
+    if command == QUIT:
+        client_socket.sendall(f"4{MESSAGE_SEP}".encode())
+        return QUIT
+
+    # if the command is 'calculate' in the correct format then send the parsed arguments
+    elif re.match(CALCULATE_COMMAND_REGEX, command):
+        command_info_str = "".join(re.findall(CALCULATE_COMMAND_REGEX, command)[0])
+        client_socket.sendall(f"1 {command_info_str}{MESSAGE_SEP}")
+
+    # if the command is max and in the correct format than parse it to format server is expecting
+    elif re.match(MAX_COMMAND_REGEX, command):
+        command_parsed = command.replace("max: (", "").rstrip(")").replace(" ", ",")
+        client_socket.sendall(f"2 {command_parsed}{MESSAGE_SEP}")
+
+    elif re.match(FACTORS_COMMAND_REGEX, command):
+        command_parsed = command.replace("factors: ", "")
+        client_socket.sendall(f"3 {command_parsed}{MESSAGE_SEP}")
+
+    else:
+        print("Got invalid command from user\n Exiting...")
+        exit(1)
+
 
 def main():
     # Default values
@@ -39,6 +69,10 @@ def main():
         try:
             sock.connect((hostname, port))
             handle_auth(sock)
+            while True:
+                # If we get return value this means we got quit and therefore we should stop
+                if execute_command():
+                    break
 
         except Exception as e:
             print(f"Could not establish connection to server: {e}")
