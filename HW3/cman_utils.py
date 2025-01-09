a12de7622ad02b02bd88b76b750d369d39d3377d
Aviv jan <avivjan2000@gmail.com>
2025-01-09 23:20:30 +0200

    add implemetation of listener that runs in a background thread

--- HW3/cman_utils.py
+++ HW3/cman_utils.py
@@ -1,5 +1,9 @@
 import sys
 import pynput, time
+from queue import Queue
+from pynput import keyboard
+
+pressed_keys = Queue()
 
 def _flush_input():
     """
@@ -60,3 +64,21 @@ def clear_print(*args, **kwargs):
     """
     print("\033[H\033[J", end="")
     print(*args, **kwargs)
+
+
+# Key listener function
+def key_listener(keys_filter=None):
+    """
+    Listens for key presses and adds them to the key queue. Utilizes `_flush_input` to clear stale input.
+    """
+    def on_press(key):
+        try:
+            char = key.char  # Get the character of the key
+            if keys_filter is None or char in keys_filter:
+                if char not in list(pressed_keys.queue):  # Avoid duplicates
+                    pressed_keys.put(char)
+        except AttributeError:
+            pass  # Handle special keys if necessary
+    _flush_input()
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()  # Keep the listener running
